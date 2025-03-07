3b680d63567d7bf4c5a019c834c22afef98fd396
fewensa <osuni@protonmail.com>
2025-03-07 06:19:35 +0000

    test api

--- packages/web/src/app/api/auth/nonce/route.ts
+++ packages/web/src/app/api/auth/nonce/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
 import * as CryptoJS from "crypto-js";
 import { Resp } from "@/types/api";
 
 export async function POST() {
   const nonce = CryptoJS.lib.WordArray.random(64).toString(CryptoJS.enc.Hex);
-  const cookieStore = await cookies();
-  cookieStore.set("x-degov-nonce", nonce, { secure: true, httpOnly: true });
+  // const cookieStore = await cookies();
+  // cookieStore.set("x-degov-nonce", nonce, { secure: true, httpOnly: true });
   return NextResponse.json(Resp.ok({ nonce }));
 }
--- packages/web/src/hooks/useApi.ts
+++ packages/web/src/hooks/useApi.ts
@@ -1,20 +1,20 @@
-import { useQuery } from "@tanstack/react-query";
-import { useState, useCallback, useEffect, useMemo } from "react";
-import axios from 'axios';
+// import { useQuery } from "@tanstack/react-query";
+// import { useState, useCallback, useEffect, useMemo } from "react";
+// import axios from 'axios';
 
-export function useApi() {
+// export function useApi() {
 
-  // const [currentToken, setCurrentToken] = useState('');
+//   // const [currentToken, setCurrentToken] = useState('');
 
-  const { refetch, ...proposalsQuery } = useQuery({
-    ['query-nonce'],
-    async () => {
+//   const { refetch, ...proposalsQuery } = useQuery({
+//     ['query-nonce'],
+//     async () => {
 
-    // const response = await axios.post('http://httpbin.org/anything');
-    // console.log('------->', 'connected', response);
-    },
-  });
+//     // const response = await axios.post('http://httpbin.org/anything');
+//     // console.log('------->', 'connected', response);
+//     },
+//   });
 
-}
+// }
 
-export default useApi;
+// export default useApi;
