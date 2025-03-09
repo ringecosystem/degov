5972f903c44df15766c8bfc504627eb8fbf587f9
fewensa <osuni@protonmail.com>
2025-03-09 14:45:47 +0000

    fake code

--- packages/web/src/hooks/useApi.ts
+++ packages/web/src/hooks/useApi.ts
@@ -1,20 +1,111 @@
-// import { useQuery } from "@tanstack/react-query";
-// import { useState, useCallback, useEffect, useMemo } from "react";
-// import axios from 'axios';
+import { useQuery } from "@tanstack/react-query";
 
-// export function useApi() {
+// fake getNonce function
+// /api/auth/nonce
+const getNonce = (): Promise<{ data: { nonce: number } }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          nonce: Math.floor(Math.random() * 1000000),
+        },
+      });
+    }, 1000);
+  });
+};
 
-//   // const [currentToken, setCurrentToken] = useState('');
+// /api/auth/login
+const login = (): Promise<{ data: { nonce: number } }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          nonce: Math.floor(Math.random() * 1000000),
+        },
+      });
+    }, 1000);
+  });
+};
 
-//   const { refetch, ...proposalsQuery } = useQuery({
-//     ['query-nonce'],
-//     async () => {
+// GET /api/profile/0x3425235
+const fetchProfile = (): Promise<{ data: { nonce: number } }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          nonce: Math.floor(Math.random() * 1000000),
+        },
+      });
+    }, 1000);
+  });
+};
 
-//     // const response = await axios.post('http://httpbin.org/anything');
-//     // console.log('------->', 'connected', response);
-//     },
-//   });
 
-// }
+// POST /api/profile/0x3425235
+const updateProfile = (): Promise<{ data: { nonce: number } }> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          nonce: Math.floor(Math.random() * 1000000),
+        },
+      });
+    }, 1000);
+  });
+};
 
-// export default useApi;
+export function useApi() {
+  const { refetch, ...proposalsQuery } = useQuery({
+    queryKey: ["query-nonce"],
+    queryFn: async () => {
+      const response = await getNonce();
+      return response?.data?.nonce;
+    },
+    enabled: true,
+  });
+
+  return {
+    refetch,
+    ...proposalsQuery,
+  };
+}
+
+export default useApi;
+
+// how to use the useApi hook
+// must use the refetch function to get the data
+// the data is not updated automatically
+
+// const { refetch, ...proposalsQuery } = useApi();
+// console.log(proposalsQuery);
+
+// example
+
+// ```tsx
+// const { refetch, ...proposalsQuery } = useApi();
+// console.log(proposalsQuery);
+
+// const handleRefetch = async () => {
+//   const data = await refetch();
+//   console.log(data);
+// };
+
+// // use the handleRefetch function to refetch the data
+// handleRefetch();
+// ```;
+
+// example 2
+
+// ```tsx
+// const { refetch, data, isLoading, isFetching, isError } = useApi();
+// console.log(data);
+
+// const handleRefetch = async () => {
+//   refetch();
+// };
+
+// console.log(data);
+
+// // use the handleRefetch function to refetch the data
+// handleRefetch();
+// ```;
