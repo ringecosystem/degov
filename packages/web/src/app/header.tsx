b579e1077e0ad2376ae132e914843812bd35993e
snoopy1412 <snoopy1412.su@gmail.com>
2025-03-13 22:46:41 +0800

    update border color

--- packages/web/src/app/header.tsx
+++ packages/web/src/app/header.tsx
@@ -2,7 +2,7 @@ import { ConnectButton } from "@/components/connect-button";
 
 export const Header = () => {
   return (
-    <header className="w-full border-b border-border bg-background px-[30px] py-[20px]">
+    <header className="w-full border-b border-[#474747] bg-background px-[30px] py-[20px]">
       <div className="flex items-center justify-between">
         <div className="invisible"></div>
         <ConnectButton />
