32405b11b4240e88171c84f83d2aa253eae3fd36
snoopy1412 <snoopy1412.su@gmail.com>
2025-04-30 16:44:05 +0800

    Enhance Markdown paste functionality

--- packages/web/src/components/editor/tiptap-extension/markdown-extension.ts
+++ packages/web/src/components/editor/tiptap-extension/markdown-extension.ts
@@ -11,16 +11,26 @@ export const MarkdownPaste = TiptapLink.extend({
         key: new PluginKey("markdownPaste"),
         props: {
           handlePaste: (view, event) => {
-            const clipboardText = event.clipboardData?.getData("text/plain");
+            const isInCodeBlock = this.editor.isActive("codeBlock");
 
-            if (clipboardText) {
-              const html = marked(clipboardText);
-              this.editor.commands.insertContent(html);
-
-              return true;
+            if (isInCodeBlock) {
+              return false;
             }
 
-            return false;
+            const clipboardText = event.clipboardData?.getData("text/plain");
+            if (!clipboardText) {
+              return false;
+            }
+
+            try {
+              const html = marked(clipboardText);
+              this.editor.commands.insertContent(html);
+              return true;
+            } catch (error) {
+              console.error("markdown paste error:", error);
+              this.editor.commands.insertContent(clipboardText);
+              return true;
+            }
           },
         },
       }),
