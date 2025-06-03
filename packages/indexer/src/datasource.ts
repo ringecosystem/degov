1aaa6a150cd391f2c8e418098476d9c795de4426
fewensa <osuni@protonmail.com>
2025-06-03 03:30:17 +0000

    feat: refactor data source handling and enhance EVM processor configuration

--- packages/indexer/src/datasource.ts
+++ packages/indexer/src/datasource.ts
@@ -1,3 +1,8 @@
+import { setTimeout } from "timers/promises";
+import { promises as fs } from "fs";
+import * as path from "path";
+import * as yaml from "yaml";
+
 export interface IndexerProcessorConfig {
   chainId: number;
   rpc: string;
@@ -9,7 +14,7 @@ export interface IndexerProcessorConfig {
 
   logs: IndexerWatchLog[];
 
-  state: IndexerProcessorState,
+  state: IndexerProcessorState;
 }
 
 export interface IndexerWatchLog {
@@ -30,10 +35,105 @@ export interface IndexerProcessorState {
 export type ContractName = "governor" | "governorToken";
 
 export class DegovDataSource {
-  // private config: IndexerProcessorConfig;
+  constructor(private readonly _config: IndexerProcessorConfig) {}
 
-  constructor() {
+  static async fromDegovConfig(
+    degovConfig: string
+  ): Promise<IndexerProcessorConfig> {
+    const dcds = new DegovConfigDataSource(degovConfig);
+    return await dcds.processorConfig();
+  }
+}
+
+class DegovConfigDataSource {
+  constructor(private readonly config: string) {}
+
+  async processorConfig(): Promise<IndexerProcessorConfig> {
+    const raw = await this.readDegovConfigRaw();
+    const dds = this.packDataSource(raw);
+    return dds;
   }
 
+  private packDataSource(rawDegovConfig: string): IndexerProcessorConfig {
+    const degovConfig = yaml.parse(rawDegovConfig);
+    const { chain, indexer, contracts } = degovConfig;
+    let rpcs = chain.rpcs ?? [];
+    if (indexer.rpc) {
+      rpcs = [indexer.rpc, ...rpcs];
+    }
+    if (!rpcs || rpcs.length === 0) {
+      throw new Error("no rpc found in degov config");
+    }
 
+    const contractNames = Object.keys(contracts);
+    const indexContracts: IndexerContract[] = contractNames.map((item) => {
+      const c = contracts[item];
+      const addr = c.address ? c.address : c;
+      return {
+        name: item,
+        address: addr,
+        standard: c.standard,
+      } as IndexerContract;
+    });
+    const indexLog: IndexerWatchLog = {
+      startBlock: indexer.startBlock,
+      endBlock: indexer.endBlock,
+      contracts: indexContracts,
+    };
+
+    const ipc: IndexerProcessorConfig = {
+      chainId: chain.id,
+      rpc: rpcs[0],
+      finalityConfirmation: indexer.finalityConfirmation ?? 50,
+      capacity: indexer.capacity ?? 30,
+      maxBatchCallSize: indexer.maxBatchCallSize ?? 200,
+      gateway: indexer.gateway,
+      logs: [indexLog],
+      state: {
+        running: true,
+      } as IndexerProcessorState,
+    };
+    return ipc;
+  }
+
+  private async readDegovConfigRaw(): Promise<string> {
+    let degovConfigRaw;
+    let times = 0;
+    while (true) {
+      times += 1;
+      if (times > 3) {
+        throw new Error("cannot read config file");
+      }
+
+      try {
+        if (
+          this.config.startsWith("http://") ||
+          this.config.startsWith("https://")
+        ) {
+          // read from http
+          const response = await fetch(this.config);
+          if (!response.ok) {
+            throw new Error(
+              `failed to load config, http error! status: ${response.status}`
+            );
+          }
+          degovConfigRaw = await response.text();
+          break;
+        } else {
+          // read from file system
+          const filePath = path.isAbsolute(this.config)
+            ? this.config
+            : path.join(process.cwd(), this.config);
+          await fs.access(filePath); // Check if file exists
+          degovConfigRaw = await fs.readFile(filePath, "utf-8");
+          break;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+
+      await setTimeout(1000);
+    }
+    return degovConfigRaw;
+  }
 }
--- packages/indexer/src/main.ts
+++ packages/indexer/src/main.ts
@@ -1,64 +1,73 @@
 import { TypeormDatabase } from "@subsquid/typeorm-store";
-import { DegovConfig, DegovConfigIndexLog, DegovConfigNanny } from "./config";
 import { GovernorHandler } from "./handler/governor";
 import { TokenHandler } from "./handler/token";
 import { EvmBatchProcessor } from "@subsquid/evm-processor";
 import { evmFieldSelection } from "./types";
+import { DegovDataSource, IndexerProcessorConfig } from "./datasource";
 
 async function main() {
-  const nanny = new DegovConfigNanny();
-  const config = await nanny.load();
-  await runProcessorEvm(config);
+  const degovConfigPath = process.env.DEGOV_CONFIG_PATH;
+  if (!degovConfigPath) {
+    throw new Error("DEGOV_CONFIG_PATH not set");
+  }
+  const processorConfig = await DegovDataSource.fromDegovConfig(
+    degovConfigPath
+  );
+  await runProcessorEvm(processorConfig);
 }
 
-async function runProcessorEvm(config: DegovConfig) {
+async function runProcessorEvm(processorConfig: IndexerProcessorConfig) {
   const processor = new EvmBatchProcessor()
+    .setFields(evmFieldSelection)
     .setRpcEndpoint({
       // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
-      capacity: 30,
-      maxBatchCallSize: 200,
-      url: config.endpoint.rpcs[0],
-    })
-    .setFields(evmFieldSelection)
-    .setFinalityConfirmation(10);
-  if (config.gateway) {
-    processor.setGateway(config.gateway);
+      url: processorConfig.rpc,
+      capacity: processorConfig.capacity ?? 30,
+      maxBatchCallSize: processorConfig.maxBatchCallSize ?? 200,
+    });
+  if (processorConfig.gateway) {
+    processor.setGateway(processorConfig.gateway);
   }
-  const indexLog: DegovConfigIndexLog = config.indexLog;
-
-  processor.addLog({
-    range: { from: indexLog.startBlock },
-    address: indexLog.contracts.map((item) => item.address),
-    transaction: true,
-  });
+  processor.setFinalityConfirmation(processorConfig.finalityConfirmation ?? 50);
+  const logs = processorConfig.logs;
+  for (const log of logs) {
+    processor.addLog({
+      range: { from: log.startBlock, to: log.endBlock },
+      address: log.contracts.map((item) => item.address),
+      transaction: true,
+    });
+  }
 
   processor.run(
     new TypeormDatabase({ supportHotBlocks: true }),
     async (ctx) => {
       for (const c of ctx.blocks) {
         for (const event of c.logs) {
-          const indexContract = indexLog.contracts.find(
-            (item) => item.address.toLowerCase() === event.address.toLowerCase()
-          );
-          if (!indexContract) {
-            continue;
-          }
-
-          try {
-            switch (indexContract.name) {
-              case "governor":
-                await new GovernorHandler(ctx).handle(event);
-                break;
-              case "governorToken":
-                await new TokenHandler(ctx, indexContract).handle(event);
-                break;
-            }
-          } catch (e) {
-            ctx.log.warn(
-              // indexContract
-              `(evm) unhandled contract ${indexContract.name} at ${event.block.height} ${event.transactionHash}, reason: ${e}, stopped from ${ctx.blocks[0].header.height} block`
+          for (const indexLog of logs) {
+            const indexContract = indexLog.contracts.find(
+              (item) =>
+                item.address.toLowerCase() === event.address.toLowerCase()
             );
-            throw e;
+            if (!indexContract) {
+              continue;
+            }
+
+            try {
+              switch (indexContract.name) {
+                case "governor":
+                  await new GovernorHandler(ctx).handle(event);
+                  break;
+                case "governorToken":
+                  await new TokenHandler(ctx, indexContract).handle(event);
+                  break;
+              }
+            } catch (e) {
+              ctx.log.warn(
+                // indexContract
+                `(evm) unhandled contract ${indexContract.name} at ${event.block.height} ${event.transactionHash}, reason: ${e}, stopped from ${ctx.blocks[0].header.height} block`
+              );
+              throw e;
+            }
           }
         }
       }
