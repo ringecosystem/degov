e66ac20573318ffddd94f6bb907d178057b70783
Bear Wang <boundless.forest@outlook.com>
2025-06-30 15:06:58 +0800

    Revert "Wrapped token demo (#202)" (#204)
    This reverts commit 405c4869a88a3e2167c602c8921bf9764104e40f.

--- contracts/src/FT.sol
+++ contracts/src/FT.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
-
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
--- contracts/src/GTK.sol
+++ contracts/src/GTK.sol
@@ -2,18 +2,18 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import {IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {ERC20Wrapper} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
-    constructor(IERC20 wrappedToken)
-        ERC20("Governance Token", "gTK")
-        ERC20Permit("Governance Token")
-        ERC20Wrapper(wrappedToken)
-    {}
+contract GTK is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner) ERC20("Governance Token", "GTK") Ownable(initialOwner) ERC20Permit("Governance Token") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
     function clock() public view override returns (uint48) {
         return uint48(block.timestamp);
@@ -24,15 +24,13 @@ contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
         return "mode=timestamp";
     }
 
-    function decimals() public view override(ERC20, ERC20Wrapper) returns (uint8) {
-        return super.decimals();
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
     }
 
-    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
-        super._update(from, to, amount);
-    }
-
-    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
 }
