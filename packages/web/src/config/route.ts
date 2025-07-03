0a9181dec824535c7c2fbc5f4d77e1f3aa73673c
snoopy1412 <snoopy1412.su@gmail.com>
2025-07-03 14:18:16 +0800

    Add apps configuration and UI components (#214)

--- packages/web/src/config/route.ts
+++ packages/web/src/config/route.ts
@@ -4,23 +4,27 @@
 
 export const routes = [
   {
-    key: 'dashboard',
-    pathname: '/'
+    key: "dashboard",
+    pathname: "/",
   },
   {
-    key: 'proposals',
-    pathname: '/proposals'
+    key: "proposals",
+    pathname: "/proposals",
   },
   {
-    key: 'delegates',
-    pathname: '/delegates'
+    key: "delegates",
+    pathname: "/delegates",
   },
   {
-    key: 'profile',
-    pathname: '/profile'
+    key: "profile",
+    pathname: "/profile",
   },
   {
-    key: 'treasury',
-    pathname: '/treasury'
-  }
+    key: "treasury",
+    pathname: "/treasury",
+  },
+  {
+    key: "apps",
+    pathname: "/apps",
+  },
 ] as const;
