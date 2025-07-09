17a9a963ee2f2cea825f97cce25a50a6ad50b553
snoopy1412 <snoopy1412.su@gmail.com>
2025-07-09 22:19:45 +0800

    Enhance Proposals component with new proposal button

--- packages/web/src/app/_components/proposals.tsx
+++ packages/web/src/app/_components/proposals.tsx
@@ -2,14 +2,19 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
 
 import { Faqs } from "@/components/faqs";
 import { ProposalsTable } from "@/components/proposals-table";
 import { Button } from "@/components/ui/button";
 import { useDaoConfig } from "@/hooks/useDaoConfig";
 
+
 export const Proposals = () => {
   const daoConfig = useDaoConfig();
+  const { isConnected } = useAccount();
+  const router = useRouter();
 
   return (
     <div className="flex flex-col gap-[20px]">
@@ -17,32 +22,56 @@ export const Proposals = () => {
         <div className="flex-1 flex flex-col gap-[10px]">
           <div className="flex items-center justify-between">
             <h3 className="text-[18px] font-extrabold">Proposals</h3>
-            {daoConfig?.offChainDiscussionUrl ? (
-              <Button className="rounded-[100px] cursor-pointer" asChild>
-                <Link
-                  href={daoConfig?.offChainDiscussionUrl}
-                  className="flex items-center gap-[5px]"
-                  target="_blank"
-                  rel="noopener noreferrer"
+            <div className="flex items-center gap-[10px]">
+              {daoConfig?.offChainDiscussionUrl ? (
+                <Button className="rounded-[100px] cursor-pointer" asChild>
+                  <Link
+                    href={daoConfig?.offChainDiscussionUrl}
+                    className="flex items-center gap-[5px]"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Image
+                      src="/assets/image/light/discussion.svg"
+                      alt="plus"
+                      width={20}
+                      height={20}
+                      className="dark:hidden"
+                    />
+                    <Image
+                      src="/assets/image/discussion.svg"
+                      alt="plus"
+                      width={20}
+                      height={20}
+                      className="hidden dark:block"
+                    />
+                    Join Discussion
+                  </Link>
+                </Button>
+              ) : null}
+              {isConnected ? (
+                <Button
+                  className="flex items-center gap-[5px] rounded-[100px]"
+                  onClick={() => router.push("/proposals/new")}
                 >
                   <Image
-                    src="/assets/image/light/discussion.svg"
+                    src="/assets/image/light/plus.svg"
                     alt="plus"
                     width={20}
                     height={20}
-                    className="dark:hidden"
+                    className="size-[20px] block dark:hidden"
                   />
                   <Image
-                    src="/assets/image/discussion.svg"
+                    src="/assets/image/plus.svg"
                     alt="plus"
                     width={20}
                     height={20}
-                    className="hidden dark:block"
+                    className="size-[20px] hidden dark:block"
                   />
-                  Join Discussion
-                </Link>
-              </Button>
-            ) : null}
+                  New Proposal
+                </Button>
+              ) : null}
+            </div>
           </div>
           <ProposalsTable type="active" />
         </div>
