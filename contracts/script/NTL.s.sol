fb217cfa33be3deebb2e08662c214cccb64f2f15
echo <hujw77@gmail.com>
2025-07-10 15:09:01 +0800

    deploy

--- contracts/script/NTL.s.sol
+++ contracts/script/NTL.s.sol
@@ -8,7 +8,7 @@ import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
 contract NoTimelockCase {
     address deployer = 0x0f14341A7f464320319025540E8Fe48Ad0fe5aec;
     IVotes GTK = IVotes(0xbC9f58566810F7e853e1eef1b9957ac82F9971df);
-    address DGOV = 0xf81b2a17aFc7f25cB53B0d7d38bE7cEE22B63C33;
+    address DGOV = 0xC63399eAb68Fd737E7B6DdCA1A43c18f32d0f73c;
 
     function _run() public {
         DGovernorWithoutTimelock dgov = new DGovernorWithoutTimelock(GTK);
