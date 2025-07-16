e0991c3b4898f4066e1c9c16de337eb4453fc7e5
snoopy1412 <snoopy1412.su@gmail.com>
2025-07-16 09:48:15 +0800

    Refactor icon URL handling in IndexerStatus and SafeTable (#243)

--- packages/web/src/utils/index.ts
+++ packages/web/src/utils/index.ts
@@ -2,5 +2,6 @@ export * from "./abi";
 export * from "./address";
 export * from "./date";
 export * from "./helpers";
+export * from "./icon";
 export * from "./markdown";
 export * from "./number";
