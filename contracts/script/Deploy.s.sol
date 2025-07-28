209ea6f66a41aba7f951acb151b96003f1286a2a
echo <hujw77@gmail.com>
2025-07-28 10:13:23 +0800

    block number case

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {safeconsole} from "forge-std/safeconsole.sol";
-import {NoTimelockCase} from "./NTL.s.sol";
+import {BlocknumberCase} from "./BN.s.sol";
 
-contract Deploy is Script, NoTimelockCase {
+contract Deploy is Script, BlocknumberCase {
     function run() public {
         safeconsole.log("Chain Id: ", block.chainid);
         vm.startBroadcast();
