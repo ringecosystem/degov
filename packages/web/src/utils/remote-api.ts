c97649747357a76271bca2644cf0558d399132c0
fewensa <37804932+fewensa@users.noreply.github.com>
2025-08-08 12:03:33 +0800

    Remove NEXT_PUBLIC_ (#324)

--- packages/web/src/utils/remote-api.ts
+++ packages/web/src/utils/remote-api.ts
@@ -1,15 +1,15 @@
 export const isRemoteApiConfigured = () => {
-  return !!process.env.NEXT_PUBLIC_DEGOV_API;
+  return !!process.env.DEGOV_API;
 };
 
 export const buildRemoteApiUrl = (): string | undefined => {
-  const daoName = process.env.NEXT_PUBLIC_DEGOV_DAO;
+  const daoName = process.env.DEGOV_DAO;
 
   if (!isRemoteApiConfigured()) {
     return undefined;
   }
 
   return daoName
-    ? `${process.env.NEXT_PUBLIC_DEGOV_API}/dao/config/${daoName}?format=yml`
-    : `${process.env.NEXT_PUBLIC_DEGOV_API}/dao/config?format=yml`;
+    ? `${process.env.DEGOV_API}/dao/config/${daoName}?format=yml`
+    : `${process.env.DEGOV_API}/dao/config?format=yml`;
 };
