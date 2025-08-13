406ce9b793970221019df0d326410ccb51ec8752
snoopy1412 <snoopy1412.su@gmail.com>
2025-08-13 15:48:04 +0800

    Update footer in Aside component (#341)
    * Update footer in Aside component
    
    * Remove unused code
    
    * Update Aside component footer to link to DeGov.AI
    
    * Fix footer placement of DeGov.AI link in Aside component

--- packages/web/src/app/aside.tsx
+++ packages/web/src/app/aside.tsx
@@ -11,12 +11,9 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { INDEXER_CONFIG } from "@/config/indexer";
-import { socialConfig } from "@/config/social";
 import { useBlockSync } from "@/hooks/useBlockSync";
 import { useDaoConfig } from "@/hooks/useDaoConfig";
 import { cn } from "@/lib/utils";
-
-import packageInfo from "../../package.json";
 
 import { Nav } from "./nav";
 
@@ -167,7 +164,7 @@ export const Aside = () => {
           <Nav collapsed={collapsed} />
         </div>
 
-        <footer className="space-y-[16px]  duration-300">
+        <footer className="space-y-[16px] duration-300">
           {collapsed ? (
             <div className="flex justify-center">
               <Tooltip>
@@ -197,59 +194,29 @@ export const Aside = () => {
               indexedBlock={indexedBlock}
             />
           )}
-
-          <div
-            className={`flex items-center gap-[10px] ${
-              collapsed ? "flex-col" : "justify-around"
-            }`}
+          <p
+            className={cn(
+              "text-xs text-muted-foreground flex items-center gap-[5px]",
+              collapsed ? "hidden" : "flex"
+            )}
           >
-            {socialConfig.map((social) => (
-              <Tooltip key={social.name}>
-                <TooltipTrigger asChild>
-                  <a
-                    href={
-                      social.name === "Github"
-                        ? `${social.url}v${packageInfo.version}`
-                        : social.url
-                    }
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={cn(
-                      "relative flex h-[24px]  items-center justify-center w-[24px] rounded-[36px] bg-card transition-opacity duration-300 hover:opacity-80",
-                      social.name === "Github" && "px-[5px] w-auto gap-[5px]"
-                    )}
-                  >
-                    <Image
-                      src={social.lightAssetPath}
-                      alt={social.name}
-                      width={social?.width || 24}
-                      height={social?.height || 24}
-                      className="object-contain block dark:hidden"
-                    />
-                    <Image
-                      src={social.assetPath}
-                      alt={social.name}
-                      width={social?.width || 24}
-                      height={social?.height || 24}
-                      className="object-contain hidden dark:block"
-                    />
-
-                    {social.name === "Github" && (
-                      <span className="text-xs text-muted-foreground">
-                        {packageInfo.version}
-                      </span>
-                    )}
-                  </a>
-                </TooltipTrigger>
-                <TooltipContent
-                  side="right"
-                  className={collapsed ? "" : "hidden"}
-                >
-                  {social.name}
-                </TooltipContent>
-              </Tooltip>
-            ))}
-          </div>
+            <span className="text-[12px]">Powered By</span>
+            <Link
+              href="https://degov.ai"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:opacity-80 transition-all duration-300"
+            >
+              <Image
+                src="/assets/image/bottom-logo.svg"
+                alt="logo"
+                width={16}
+                height={16}
+                priority
+              />
+            </Link>
+            <span className="text-[12px]">DeGov.AI</span>
+          </p>
         </footer>
       </div>
     </aside>
