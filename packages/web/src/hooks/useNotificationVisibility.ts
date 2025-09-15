c8bf8574c6e930857f2e5368422e90b943bf2fdb
snoopy1412 <snoopy1412.su@gmail.com>
2025-09-15 19:24:13 +0800

    Add NEXT_PUBLIC_NOTIFICATION_API to environment variables and update notification visibility hook

--- packages/web/src/hooks/useNotificationVisibility.ts
+++ packages/web/src/hooks/useNotificationVisibility.ts
@@ -4,7 +4,8 @@ import { useAccount } from "wagmi";
 export const useNotificationVisibility = () => {
   const { isConnected } = useAccount();
   const clientApi =
-    typeof window !== "undefined" ? env("NEXT_PUBLIC_DEGOV_API") : undefined;
-
+    typeof window !== "undefined"
+      ? env("NEXT_PUBLIC_NOTIFICATION_API")
+      : undefined;
   return isConnected && !!clientApi;
 };
